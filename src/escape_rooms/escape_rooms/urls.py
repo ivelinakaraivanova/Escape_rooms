270deefc6edf960260aaa1e8bfb88ff050a7dff6
ivelinakaraivanova <73067985+ivelinakaraivanova@users.noreply.github.com>
2021-08-13 19:52:49 +0300

    Update urls.py

--- src/escape_rooms/escape_rooms/urls.py
+++ src/escape_rooms/escape_rooms/urls.py
@@ -4,13 +4,9 @@ from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('django.contrib.auth.urls')),
+    path('contrib-auth/', include('django.contrib.auth.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('api/v1/accounts/', include('escape_rooms.accounts_app.urls')),
     path('api/v1/escape/', include('escape_rooms.escape_app.urls')),
     path('api/v1/organizations/', include('escape_rooms.organizations_app.urls')),
 ]
-
-# TODO --> documentation
-#  path('docs/', include_docs_urls(title='qwerty')),
-#
